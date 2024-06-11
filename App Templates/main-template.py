7b100f9fa382b2bdb5aaf871f351a9919f2ab82f
Thomas0Miller <t.miller85@gmail.com>
2024-06-10 23:55:56 -0700

    template update

--- App Templates/main-template.py
+++ App Templates/main-template.py
@@ -4,3 +4,32 @@ Created on Sat Jun 8 3:35:53 2024
 
 @author: Thomas
 """
+
+
+import os
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageDraw
+from FileApp import FileApp
+from TrayApp import TrayApp
+
+def create_icon():
+    image = Image.new('RGB', (64, 64), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+    draw.rectangle((0, 0, 63, 63), outline=(0, 0, 0))
+    return image
+
+def main():
+    root = tk.Tk()
+    app_directory = os.path.dirname(os.path.abspath(__file__))
+    settings_path = os.path.join(app_directory, 'settings.json')
+    
+    file_app = FileApp(root, settings_path)
+    icon_image = create_icon()
+    tray_app = TrayApp(file_app, settings_path, icon_image)
+    file_app.set_tray_app(tray_app)
+    
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
