77b849c0031cb6f446192dadbe31daa20c31aaef
Thomas0Miller <t.miller85@gmail.com>
2024-06-12 22:43:40 -0700

    Update ColorCatcher.py
    Added a label to accompany the preview, and an input field to view a specific unsampled color.

--- ColorCatcher.py
+++ ColorCatcher.py
@@ -14,7 +14,7 @@ Created on Fri Jun 16 07:06:23 2024
 
 VERSION = 1.0 # I am pleased with it. 
 
-# WANT: to get a crosshair of some sort into the zoomed view to identify the critical pixel
+# NEED PANIC: to get a crosshair of some sort into the zoomed view to identify the critical pixel
 
 # Could add: better export options
 
@@ -115,7 +115,7 @@ class ColorCatcher:
         instructions_text = "Instructions:\n" \
                             "1. Click 'Start' to begin catching colors.\n" \
                             "2. Hover over the desired color; press 'C'.\n" \
-                            "3. Press 'Escape' to stop catching colors.\n" \
+                            "3. Press 'Escape' or <spacebar> to stop.\n" \
                             "4. Click 'Save' to save the caught colors.\n" \
                             "Note: clicking can disrupt catching. "
         # instructions_label = tk.Text(main_frame, height=5, wrap=tk.WORD, bg="lightgray")
@@ -153,27 +153,69 @@ class ColorCatcher:
         self.use_screenshot_var = tk.BooleanVar(value=False)
         self.use_screenshot_checkbox = ttk.Checkbutton(button_frame, text="Screenshot", variable=self.use_screenshot_var)
         self.use_screenshot_checkbox.pack(side=tk.RIGHT, padx=(0, 10)) 
-        createToolTip(self.use_screenshot_checkbox, "Snap and overlay a screenshot\nto collect uncooperative samples.\nEsc or spacebar will STOP.")
+        createToolTip(self.use_screenshot_checkbox, "Snap and overlay a screenshot\n" \
+                                                    "to collect uncooperative samples.\n" \
+                                                    "Esc or spacebar will STOP.\n\n" \
+                                                    "To sample something that only\n" \
+                                                    "occurs during a click, you might\n" \
+                                                    "try holding the click and hitting\n" \
+                                                    "'PrtScr' on your keyboard. Paste\n" \
+                                                    "the result somewhere to sample."
+                                                    )
 
 
         self.paned_window = ttk.Panedwindow(main_frame, orient=tk.HORIZONTAL)
         self.paned_window.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        left_frame = ttk.Frame(self.paned_window, relief=tk.SUNKEN)
+        left_frame = ttk.Frame(self.paned_window) # , relief=tk.SUNKEN
         left_frame.pack_propagate(False)
         self.paned_window.add(left_frame, weight=1)
 
         # The Catcher Label
         self.catcher_str = tk.StringVar()
         self.catcher_str.set(f"Caught Colors: {self.score}")
-        self.catcher_label = ttk.Label(left_frame, textvariable=self.catcher_str)
+        self.catcher_label = ttk.Label(left_frame, textvariable=self.catcher_str) # , relief="sunken"
         self.catcher_label.pack(anchor=tk.CENTER, padx=5, pady=5)
 
 
-        self.color_listbox = tk.Listbox(left_frame, height=20, width=30)
+        self.color_listbox = tk.Listbox(left_frame, height=18, width=30)
         self.color_listbox.pack(fill=tk.BOTH, expand=True)
         self.color_listbox.bind('<<ListboxSelect>>', self.on_color_select)
 
+        # Color input 
+        # Add custom styles for the input frame and label to blend them into the surrounding frame
+        style = ttk.Style()
+        style.configure("Custom.TFrame", background="#f0f0f0", relief="flat")
+        style.configure("Custom.TLabel", background="#f0f0f0", relief="flat")
+
+        input_frame = ttk.Frame(left_frame, style="Custom.TFrame", relief=tk.FLAT, borderwidth=0)  # , relief="flat", borderwidth=0
+        input_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
+
+        self.color_entry = tk.Entry(input_frame, width=10)
+        self.color_entry.insert(0, "#")  # Set default text
+        self.color_entry.pack(side=tk.LEFT, padx=(0, 5))
+
+        def keep_hash(event):
+            if not self.color_entry.get().startswith("#"):
+                self.color_entry.insert(0, "#")
+        self.color_entry.bind("<FocusOut>", keep_hash)
+        self.color_entry.bind("<KeyRelease>", keep_hash)
+
+        self.color_entry.bind("<Return>", lambda event: self.send_color_to_preview())  # Bind Enter key
+
+        self.send_button = ttk.Button(input_frame, text=">", command=self.send_color_to_preview, style="Compact.TButton", width=2)
+        self.send_button.pack(side=tk.LEFT)
+
+        self.current_color_label = ttk.Label(input_frame, text="(None)", relief="flat", borderwidth=0)
+        self.current_color_label.pack(side=tk.LEFT, padx=(10, 0))
+
+
+
+
+
+
+
+        # Right frame
         self.right_frame = ttk.Frame(self.paned_window, relief=tk.SUNKEN)
         self.right_frame.pack_propagate(False)
         self.paned_window.add(self.right_frame, weight=1)
@@ -181,6 +223,7 @@ class ColorCatcher:
         self.right_paned_window = ttk.Panedwindow(self.right_frame, orient=tk.VERTICAL)
         self.right_paned_window.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+        # Zoom view
         zoomed_frame = ttk.Frame(self.right_paned_window, relief=tk.SUNKEN, borderwidth=0)
         zoomed_frame.pack_propagate(False)
         self.right_paned_window.add(zoomed_frame, weight=1)
@@ -188,6 +231,7 @@ class ColorCatcher:
         self.zoomed_canvas = tk.Canvas(zoomed_frame, bg='grey', highlightthickness=0)
         self.zoomed_canvas.pack(fill=tk.BOTH, expand=True)
 
+        # Color view
         color_frame = ttk.Frame(self.right_paned_window, relief=tk.SUNKEN, borderwidth=0)
         color_frame.pack_propagate(False)
         self.right_paned_window.add(color_frame, weight=1)
@@ -197,6 +241,7 @@ class ColorCatcher:
 
         self.color_canvas.bind("<Configure>", self.on_color_canvas_resize)
 
+        # Grid setup
         self.root.grid_rowconfigure(1, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
 
@@ -204,10 +249,8 @@ class ColorCatcher:
 
         self.root.after(100, self.center_panes)
 
-        # self.root.bind('<Escape>', self.stop_capture)
-        # self.root.bind('<space>', self.stop_capture)
-
         print("GUI setup complete.")
+
 
     def center_panes(self):
         self.root.update_idletasks()
@@ -230,7 +273,22 @@ class ColorCatcher:
                 for color in self.colors:
                     f.write(f"{color}\n")
             messagebox.showinfo("Save Successful", f"{self.score} color(s) caught successfully!")
+            # NEED PANIC- OPEN FILE AFTER SAVE!
 
+    # For previewing a specific code
+    def send_color_to_preview(self):
+        try:
+            color_hex = self.color_entry.get().strip()
+            if not color_hex.startswith("#"):
+                color_hex = f"#{color_hex}"
+            # Validate the hex color format
+            if len(color_hex) == 7 and all(c in '0123456789ABCDEFabcdef' for c in color_hex[1:]):
+                self.color_canvas.create_rectangle(0, 0, self.color_canvas.winfo_width(), self.color_canvas.winfo_height(), fill=color_hex)
+                self.current_color_label.config(text=f"Current Color: {color_hex}")
+            else:
+                raise ValueError("Invalid Color")
+        except Exception as e:
+            self.current_color_label.config(text=f"Invalid Color")
 
 
     # Select a caught color from list, see it in the canvas
@@ -242,6 +300,7 @@ class ColorCatcher:
                 color_entry = event.widget.get(index)
                 hex = color_entry.split(",")[0]
                 self.color_canvas.create_rectangle(0, 0, self.color_canvas.winfo_width(), self.color_canvas.winfo_height(), fill=hex)
+                self.current_color_label.config(text=f"{hex}")
         except Exception as e:
             print(f"Error selecting color: {e}")
 
@@ -276,10 +335,12 @@ class ColorCatcher:
             self.capturing = True
             self.stop_threads = False
             self.start_stop_button.config(text="Stop")
+            self.current_color_label.config(text="")
             self.root.config(cursor="cross")
             # self.root.bind("<Button-1>", self.catch_color)
             self.root.bind("<c>", lambda event: self.catch_color())  # Bind C to catch_color
             self.root.bind("<Escape>", self.stop_capture)
+            self.root.bind("<space>", self.stop_capture)
 
             self.root.grab_set()  # Ensure the application captures all mouse events
 
@@ -378,19 +439,6 @@ class ColorCatcher:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
     def update_zoomed_view(self):
         print("Starting update_zoomed_view thread")
         while not self.stop_threads:
@@ -435,6 +483,7 @@ class ColorCatcher:
                 self.current_color = rgb
                 self.color_canvas.delete("transparency")
                 self.color_canvas.create_rectangle(0, 0, self.color_canvas.winfo_width(), self.color_canvas.winfo_height(), fill=hex)
+                self.current_color_label.config(text=f"{hex}")
             except Exception as e:
                 print(f"Error in update_color_display: {e}")
             time.sleep(0.1)
@@ -491,17 +540,17 @@ class ColorCatcher:
         self.root.attributes("-topmost", True)
         self.root.after(200, self.lift_tool)
 
-    def create_custom_cursor(self):
-        cursor_size = 32
-        cursor_image = tk.PhotoImage(width=cursor_size, height=cursor_size)
-        cursor_image.put(("white",) * cursor_size, to=(0, 0, cursor_size, cursor_size))
-        for i in range(cursor_size):
-            cursor_image.put("black", to=(i, cursor_size // 2))
-            cursor_image.put("black", to=(cursor_size // 2, i))
-        for i in range(cursor_size // 2 - 1, cursor_size // 2 + 2):
-            for j in range(cursor_size // 2 - 1, cursor_size // 2 + 2):
-                cursor_image.put("white", to=(i, j))
-        self.root.config(cursor="crosshair")
+    # def create_custom_cursor(self):
+    #     cursor_size = 32
+    #     cursor_image = tk.PhotoImage(width=cursor_size, height=cursor_size)
+    #     cursor_image.put(("white",) * cursor_size, to=(0, 0, cursor_size, cursor_size))
+    #     for i in range(cursor_size):
+    #         cursor_image.put("black", to=(i, cursor_size // 2))
+    #         cursor_image.put("black", to=(cursor_size // 2, i))
+    #     for i in range(cursor_size // 2 - 1, cursor_size // 2 + 2):
+    #         for j in range(cursor_size // 2 - 1, cursor_size // 2 + 2):
+    #             cursor_image.put("white", to=(i, j))
+    #     self.root.config(cursor="crosshair")
 
 
 if __name__ == "__main__":
