c8ff70e2f24bb10e1ee68cb7d93d6a3061831e6c
Thomas0Miller <t.miller85@gmail.com>
2024-07-01 08:03:59 -0700

    Update ToolTipClass.py

--- ToolTipClass.py
+++ ToolTipClass.py
@@ -34,17 +34,17 @@ class ToolTip:
         self.widget = widget
         self.text = text
         self.tipwindow = None
-        self.id = None
-        self.x = self.y = 0
 
     def showtip(self):
-        # "Display text in tooltip window" 
-        x, y, cx, cy = self.widget.bbox("insert")
+        "Display text in tooltip window"
+        if self.tipwindow or not self.text:
+            return
+        x, y, _, _ = self.widget.bbox("insert")
         x = x + self.widget.winfo_rootx() + 25
-        y = y + cy + self.widget.winfo_rooty() + 25
+        y = y + self.widget.winfo_rooty() + 25
         self.tipwindow = tw = tk.Toplevel(self.widget)
         tw.wm_overrideredirect(1)
-        tw.wm_geometry("+%d+%d" % (x, y))
+        tw.wm_geometry(f"+{x}+{y}")
         label = tk.Label(tw, text=self.text, justify=tk.LEFT,
                       background="#ffffe0", relief=tk.SOLID, borderwidth=1,
                       font=("tahoma", "8", "normal"))
@@ -59,7 +59,10 @@ class ToolTip:
 def createToolTip(widget, text):
     toolTip = ToolTip(widget, text)
     def enter(event):
-        toolTip.showtip()
+        try:
+            toolTip.showtip()
+        except:
+            pass  # If showtip fails, we simply don't show the tooltip
     def leave(event):
         toolTip.hidetip()
     widget.bind('<Enter>', enter)
